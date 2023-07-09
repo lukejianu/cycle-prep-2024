e9210a26ca7d2f5d1da70852a2d49e3f43f2a746
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-07-09 18:16:54 -0500

    chore: finish b75 for real

--- neetcode/bit_manipulation/counting_bits.py
+++ neetcode/bit_manipulation/counting_bits.py
@@ -1,4 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        return [] 
-        
+        cache = [0 for _ in range(n + 1)]
+        offset = 1
+        for i in range(1, n + 1):
+            if i == offset * 2:
+                offset *= 2
+            cache[i] = 1 + cache[i - offset]
+        return cache
